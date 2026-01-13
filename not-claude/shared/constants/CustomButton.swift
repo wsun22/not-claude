1b44c21503ce7ca94be8145b29f640f4263e6767
wsun <williamsun@Williams-MacBook-Air-2.local>
2026-01-13 14:54:48 -0800

    move based on drag

--- not-claude/shared/constants/CustomButton.swift
+++ not-claude/shared/constants/CustomButton.swift
@@ -9,12 +9,40 @@ import Foundation
 import SwiftUI
 
 struct CustomButton: View {
+    @State private var width: CGFloat = 50
+    @State private var height: CGFloat = 50
+    @State private var offset: CGSize = .zero
+    @State private var isDragging: Bool = false
+    
+    let action: () -> Void
     
     var body: some View {
+        Ellipse()
+            .fill(Color.orange)
+            .frame(width: width, height: height)
+            .offset(offset)
+            .gesture(handleDrag())
+    }
+    
+    private func handleDrag() -> some Gesture {
+        DragGesture(minimumDistance: 0)
+            .onChanged { value in
+                let horizontalDistance = value.translation.width
+                let verticalDistance = value.translation.height
+                
+                offset = CGSize(width: horizontalDistance / 50, height: verticalDistance / 50)
+                
+            } .onEnded { value in
+                width = 50
+                height = 50
+                offset = .zero
+            }
         
     }
 }
 
 #Preview {
-    CustomButton()
+    CustomButton {
+        print("tapped")
+    }
 }
