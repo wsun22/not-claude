96646eae65c20a4209886bae4a86cd73dc431307
wsun <williamsun@Williams-MacBook-Air-2.local>
2026-01-13 16:00:34 -0800

    frame to reserve space depending on isDragging + ignore safe area

--- not-claude/shared/constants/CustomButton.swift
+++ not-claude/shared/constants/CustomButton.swift
@@ -19,10 +19,12 @@ struct CustomButton: View {
     var body: some View {
         Ellipse()
             .fill(Color.orange)
-            .frame(width: width * (isDragging ? 1.5 : 1.0), height: height * (isDragging ? 1.5 : 1.0))
+            .frame(width: width * (isDragging ? 1.1 : 1.0), height: height * (isDragging ? 1.1 : 1.0))
             .offset(offset)
             .coordinateSpace(name: "button")
             .gesture(handleDrag())
+            .frame(width: 50 * 1.1, height: 50 * 1.1)
+            .ignoresSafeArea()
     }
     
     private func handleDrag() -> some Gesture {
@@ -34,21 +36,13 @@ struct CustomButton: View {
                 
                 let dx = fingerLocation.x - center.x
                 let dy = fingerLocation.y - center.y
-                
-                // log only occasionally (every ~10th update based on drag distance)
-//                if abs(dx) > 10 && Int(dx) % 10 == 0 {
-//                    print("center: \(center)")
-//                    print("finger: \(fingerLocation)")
-//                    print("dx: \(dx), dy: \(dy)")
-//                    print("offset will be: \(CGSize(width: dx / 3, height: dy / 3))")
-//                    print("---")
-//                }
-                
+                                
                 offset = CGSize(width: dx / 30, height: dy / 30)
                 width = 50 + abs(dx) / 50
                 height = 50 + abs(dy) / 50
             } .onEnded { value in
                 defer { isDragging = false }
+                
                 let dragDistance = sqrt(pow(offset.width, 2) + pow(offset.height, 2)) // handle taps
                 if dragDistance < 1 {
                     action()
